cb6ff7987ccc783ac7a890db8d35a50538a8a5f9
Abdullah Al Rafi <rafiabdps@gmail.com>
2024-12-15 18:16:28 +0600

    Create BaseNegativeTestMissingElement.java

---
+++ Base/BaseNegativeTestMissingElement.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class BaseNegativeTestMissingElement {
+    public static void main(String[] args) {
+        // Setting up WebDriver
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("user-data-dir=C:\\Users\\Administrator\\AppData\\Local\\Google\\Chrome\\User Data");
+        options.addArguments("profile-directory=Default");
+        WebDriver driver = new ChromeDriver(options);
+
+        // Open the Base.html page
+        driver.get("http://localhost:8000/base");
+
+        // Maximize window
+        driver.manage().window().maximize();
+
+       // Negative Test: Check for a missing element
+        try {
+            driver.findElement(By.id("nonexistentElement"));
+            System.out.println("Negative Test - Non-existent element found: Failed");
+        } catch (NoSuchElementException e) {
+            System.out.println("Negative Test - Non-existent element found: Passed");
+        }
+
+        
+
+        driver.quit();
+    }
+}
